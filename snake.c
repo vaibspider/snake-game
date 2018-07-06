573dff0363d05cd091499ff7af938a0b9ac0af19
Vaibhav Kurhe <vaibhav.kurhe@gmail.com>
2018-07-06 17:36:57 +0530

    LEFT to UP

--- snake.c
+++ snake.c
@@ -2,9 +2,17 @@
 #include <string.h>
 #include <unistd.h>
 
+typedef struct RowCol {
+    int r;
+    int c;
+} RowCol;
+
+char snake[1024] = "++++++++++";
+RowCol rowcol[1024];
+void init_row_col(int, int);
+
 int main() {
 
-    char snake[1024] = "SSSSSSSSSS";
     char object = 'A';
     int row, col;
     int ch;
@@ -19,6 +27,11 @@ int main() {
     int row_max;
     int col_max;
     int i;
+    int j;
+    int pointer;
+    int curr_row;
+    int curr_col;
+    int just_entered;
 
     initscr();
     cbreak();
@@ -39,6 +52,7 @@ int main() {
 
     object_row = row/4;
     object_col = col/8;
+
 
     mvprintw(snake_row, snake_col, "%s", snake);
     mvaddch(object_row, object_col, object | A_BOLD);
@@ -64,11 +78,13 @@ int main() {
                     //refresh();
                     //usleep(2000000);
                     if (input == KEY_UP || input == KEY_DOWN) {
+                        just_entered = 1;
                         break;
                     }
                     clear();
                     mvaddch(object_row, object_col, object | A_BOLD);
                     snake_col--;
+                    init_row_col(snake_row, snake_col);
                     mvprintw(snake_row, snake_col, "%s", snake);
                     refresh();
                 }
@@ -130,22 +146,50 @@ int main() {
                     //mvprintw(LINES - 2, 0, "inside while loop");
                     //refresh();
                     //sleep(1);
-                    usleep(100000);
-                    input = getch();
                     //mvprintw(LINES - 2, 0, "Input = %d\n", input);
                     //refresh();
                     //usleep(2000000);
+                    
+                    //curr_row = snake_row;
+                    //curr_col = snake_col;
+
+                    //snake_row--;
+                    if (just_entered == 1) {
+                        pointer = 0;
+                        for (i = 0; i < strlen(snake) - 1 && pointer < strlen(snake) - 1; i++) {
+                            clear();
+                            mvaddch(object_row, object_col, object | A_BOLD);
+                            //curr_row = snake_row;
+                            //curr_col = snake_col;
+                            for (j = 0; j <= pointer; j++) {
+                                mvaddch(--rowcol[j].r, rowcol[j].c, snake[j]);
+                            }
+
+
+                            for (j = pointer + 1; j < strlen(snake); j++) {
+                                mvaddch(rowcol[j].r, --rowcol[j].c, snake[j]);
+                                //curr_col++;
+                            }
+                            pointer++;
+                            refresh();
+                            usleep(100000);
+                            //mvaddch(snake_row - 1, snake_col, snake[i]);
+                        }
+                        just_entered = 0;
+                    }
+
+                    clear();
+                    mvaddch(object_row, object_col, object | A_BOLD);
+                    for (i = 0; i < strlen(snake); i++) {
+                        mvaddch(--rowcol[i].r, rowcol[i].c, snake[i]);
+                    }
+                    //mvprintw(snake_row, snake_col, "%s", snake);
+                    refresh();
+                    usleep(100000);
+                    input = getch();
                     if (input == KEY_LEFT || input == KEY_RIGHT) {
                         break;
                     }
-                    clear();
-                    mvaddch(object_row, object_col, object | A_BOLD);
-                    snake_row--;
-                    for (i = 0; i < strlen(snake); i++) {
-                        mvaddch(snake_row - 1, snake_col, snake[i]);
-                    }
-                    mvprintw(snake_row, snake_col, "%s", snake);
-                    refresh();
                 }
                 if (snake_row == 0) {
                     //clear();
@@ -207,4 +251,14 @@ int main() {
     getch();
     endwin();
     return 0;
+}
+
+void init_row_col(int startrow, int startcol) {
+    int i;
+    for (i = 0; i < strlen(snake); i++) {
+        rowcol[i].r = startrow + i;
+        rowcol[i].c = startcol + i;
+    }
+    rowcol[i].r = EOF;
+    rowcol[i].c = EOF;
 }
